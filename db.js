8d306af40925b9c07a4e018b012670db68322335
robin <robinnsheen@berkeley.edu>
2022-06-30 15:00:23 -0700

    changed to mac settings

--- db.js
+++ db.js
@@ -4,14 +4,14 @@
 const { Client } = require("pg");
 
 // MAC SET UP
-// const DB_URI = process.env.NODE_ENV === "test"
-//     ? "postgresql:///biztime_test"
-//     : "postgresql:///biztime";
+const DB_URI = process.env.NODE_ENV === "test"
+    ? "postgresql:///biztime_test"
+    : "postgresql:///biztime";
 
 //LINUX SET UP
-const DB_URI = process.env.NODE_ENV === "test"
-    ? "postgresql://robinnsheen:pgpassword@localhost/biztime_test"
-    : "postgresql://robinnsheen:pgpassword@localhost/biztime";
+// const DB_URI = process.env.NODE_ENV === "test"
+//     ? "postgresql://robinnsheen:pgpassword@localhost/biztime_test"
+//     : "postgresql://robinnsheen:pgpassword@localhost/biztime";
 
 let db = new Client({
   connectionString: DB_URI
